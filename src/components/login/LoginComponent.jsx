7528e17dfa72b75b540995a706d509c8131d0fa9
AnukulPK <anukulpk2@gmail.com>
2022-01-15 17:11:38 +0530

    state implemented for login

--- src/components/login/LoginComponent.jsx
+++ src/components/login/LoginComponent.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginComponent = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const userNameInputHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const passwordInputHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <>
       User Name:
-      <input type="text" name="username" />
-      Password: <input type="password" name="password" />
+      <input
+        type="text"
+        name="username"
+        value={userName}
+        onChange={userNameInputHandler}
+      />
+      Password:{' '}
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={passwordInputHandler}
+      />
       <button>Login</button>
     </>
   );
