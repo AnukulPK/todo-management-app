a96a0cd31a69b92170ebda91ba9c73549303eec8
AnukulPK <anukulpk2@gmail.com>
2022-01-16 16:36:10 +0530

    hardcoded login

--- src/components/login/LoginComponent.jsx
+++ src/components/login/LoginComponent.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 const LoginComponent = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [loginFailed, setLoginFalied] = useState(false);
 
   const userNameInputHandler = (event) => {
     setUserName(event.target.value);
@@ -12,8 +14,20 @@ const LoginComponent = () => {
     setPassword(event.target.value);
   };
 
+  const loginClickHandler = () => {
+    if (userName === 'anukul' && password === 'abcd') {
+      setLoginFalied(false);
+      setSuccessMessage(true);
+    } else {
+      setLoginFalied(true);
+      setSuccessMessage(false);
+    }
+  };
+
   return (
     <>
+      {loginFailed && <p>Invalid Credentials</p>}
+      {successMessage && <p>login Successful</p>}
       User Name:
       <input
         type="text"
@@ -28,7 +42,7 @@ const LoginComponent = () => {
         value={password}
         onChange={passwordInputHandler}
       />
-      <button>Login</button>
+      <button onClick={loginClickHandler}>Login</button>
     </>
   );
 };
