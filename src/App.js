ec437fccec5c67e13af21d307c5e22ef8a73de01
Anukul <anukul.p@growthsourceft.com>
2022-01-27 15:08:11 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
 // import TodoApp from './components/todo/TodoApp';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginComponent from './components/login/LoginComponent';
 import Welcome from './components/welcome/Welcome';
 import Error from './components/error/Error';
 import ListTodos from './components/listTodos/ListTodos';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Logout from './components/logout/Logout';
+import './bootstrap.css';
+import './App.css';
 
 function App() {
   return (
-    <div className="TodoApp">
+    <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<LoginComponent />} />
           <Route path="/login" element={<LoginComponent />} />
           <Route path="/welcome/:name" element={<Welcome />} />
           <Route path="/todos" element={<ListTodos />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="*" element={<Error />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span className="text-muted">All Rights Reserved 2018 @anukul</span>
+    </footer>
+  );
+};
+
+export default Footer;
