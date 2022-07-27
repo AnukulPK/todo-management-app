db89cd750eb7393af9d1303f43ce8165ef048947
unknown <anukulpk2@gmail.com>
2022-07-27 15:31:42 +0530

    integrating backend wip

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const Welcome = () => {
   const { name } = useParams();
 
   //   console.log(name);
 
+  const helloWorldHandler = async (name) => {
+    // const data = await fetch(`http://localhost:8080/hello-world-bean/${name}`);
+
+    console.log(name);
+  };
+
   return (
     <>
       <h1>Welcome</h1>
       <div className="container">
         Welcome {name}.You can manage todos <Link to="/todos">here</Link>
+      </div>
+      <div className="container">
+        <p>Click here to get a customised welcome message.</p>
+        <button
+          onClick={() => helloWorldHandler(name)}
+          className="btn btn-success"
+        >
+          Get Welcome Message
+        </button>
       </div>
     </>
   );
