a1fe71d22aa62a0a01719dd189a19e352801b4ec
unknown <anukulpk2@gmail.com>
2022-08-12 09:28:10 +0530

    created form with Formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
+import moment from "moment";
+import { Field, Form, Formik } from "formik";
 
 const TodoComponent = () => {
   const { id } = useParams();
-  return <div>{`TodoComponent ${id}`}</div>;
+  const [formId, setFormId] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+
+  return (
+    <div>
+      <h1>Todo</h1>
+      <div className="container">
+        <Formik>
+          {(props) => (
+            <Form>
+              <fieldset className="form-group">
+                <label>Description</label>
+                <Field
+                  className="form-control"
+                  type="text"
+                  name="description"
+                />
+              </fieldset>
+
+              <fieldset className="form-group">
+                <label>Target Date</label>
+                <Field className="form-control" type="date" name="targetDate" />
+              </fieldset>
+              <button type="submit" className="btn  btn-success">
+                Save
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default TodoComponent;
